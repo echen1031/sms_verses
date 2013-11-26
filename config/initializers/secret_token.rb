f99751f7bd6a26b9be5c5dfe3093082da274cda0
Eric Chen <ericchen@Erics-MacBook-Air.local>
2013-11-26 09:39:08 -0800

    Added devise, phone migration to database

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SmsVerses::Application.config.secret_token = '55eb0a91936df8f9bd588fcb26cca07676c1b67eeab761c5f8dab2a558c3555d0d91b2ce4ed2b86a548643b97acb17e3abf4e528d9ab1da191eaf087271ac638'
