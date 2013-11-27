f20cd97611bd2d381722688d663e5a3bac9dfd10
alex-nexus <alkchen@gmail.com>
2013-11-27 11:47:59 -0800

    add daily email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,25 @@
 class UserMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "dispenser@comingtohim.com"
 
-  def welcome_email(user)
+  def start_subscription(user, user_subscription)
     @user = user
+    @user_subscription = user_subscription
     @url = 'http://comingtohim.com/login'
-    mail(to: @user.email, subject: 'Welcome to Coming to Him')
+    mail(to: @user_subscription.email, subject: 'Welcome to Coming to Him')
+  end
+
+  def end_subscription(user, user_subscription)
+    @user = user
+    @user_subscription = user_subscription
+    @url = 'http://comingtohim.com/login'
+    mail(to: @user_subscription.email, subject: 'Goodbye')
+  end
+
+  def daily
+  	@user = user
+    @user_subscription = user_subscription
+    @verse = ''
+    @url = 'http://comingtohim.com/login'
+    mail(to: @user_subscription.email, subject: 'Daily Verse')
   end
 end
