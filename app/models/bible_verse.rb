b40b515027882165e864dd3c24af388d3f95f134
alex-nexus <alkchen@gmail.com>
2013-11-26 22:51:07 -0800

    load csv into db

--- app/models/bible_verse.rb
+++ app/models/bible_verse.rb
@@ -2,12 +2,13 @@ require 'csv'
 
 class BibleVerse < ActiveRecord::Base
   attr_accessible :book, :chapter_num, :content, :verse_num, :book_num, :reference
-  validates_presence_of :book, :chapter_num, :content, :verse_num, :book_num, :referece
+  validates_presence_of :book, :chapter_num, :content, :verse_num, :book_num, :reference
   validates_numericality_of :chapter_num, :verse_num, :book_num
 
   def self.load_from_fixture
-  	CSV.foreach("config/fixtures/verses.txt", {:col_sep => "\t"}) do |row|
-  		binding.pry  		
+  	CSV.foreach("config/fixtures/verses.txt", {:col_sep => "\t", :headers => true}) do |row|
+  		verse_hash = row.to_hash.reject{|k,v|['REFID', 'memorize'].include? k}
+  		BibleVerse.create(verse_hash)  		
 	end
   end
 end
