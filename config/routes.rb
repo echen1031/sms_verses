b8d18738e6d3986509478e38761407e3aac66eca
Eric Chen <ericchen@erics-air.gateway.pace.com>
2013-11-28 23:24:04 -0800

    added routes and html landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 SmsVerses::Application.routes.draw do
 
+  root :to => 'home#index'
   resources :bible_verses
 
   devise_for :users
@@ -54,7 +55,6 @@ SmsVerses::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'user_subscriptions#index'
 
   # See how all your routes lay out with "rake routes"
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,3 @@
 puts 'load bible verses'
+BibleVerse.delete_all
 BibleVerse::load_from_file
