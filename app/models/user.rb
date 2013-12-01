574cfa50af398dd62e976df050e0424de6d5cf25
Eric Chen <eric87chen@gmail.com>
2013-12-01 02:35:27 -0800

    added validations to models

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,5 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
-  has_many :user_subscriptions
+  has_one :user_subscriptions
 end
