c9cdb33fb0cadd0e5c31118e5955e80d5b3f729c
George Kuan <commonlisp@gmail.com>
2013-12-13 09:30:44 -0800

    The Devise form prepopulates with the Timezone determined by Geonames web service

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :authenticate_user!
 
+  Timezone::Configure.begin do |c|
+    # TODO: Fill this in with a dedicated Geonames account name
+    c.username = 'commonlisp' 
+  end
+  
   def after_sign_in_path_for(resource)
     session[root_path] || user_user_subscriptions_path(current_user.id)
   end
