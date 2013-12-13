ccbd1b6107eeb62b1b9bca936b89ec4224cebea6
Alex Chen <alexchen@Alexs-MacBook-Air.local>
2013-12-13 01:48:24 -0800

    remove phone carrier

--- db/migrate/20131213072424_add_phone_carrier_to_user_subscription.rb
+++
@@ -1,5 +0,0 @@
-class AddPhoneCarrierToUserSubscription < ActiveRecord::Migration
-  def change
-    add_column :user_subscriptions, :phone_carrier, :string
-  end
-end
--- db/migrate/20131213072642_add_phone_carrier_id_to_user_subscription.rb
+++
@@ -1,6 +0,0 @@
-class AddPhoneCarrierIdToUserSubscription < ActiveRecord::Migration
-  def change
-    add_column :user_subscriptions, :phone_carrier_id, :integer
-    remove_column :user_subscriptions, :phone_carrier
-  end
-end
