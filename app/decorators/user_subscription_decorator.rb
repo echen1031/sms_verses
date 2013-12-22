098509c2cea559aaaa3c283d2e03b8e65501ef36
Alex Chen <alexchen@Alexs-MacBook-Air.local>
2013-12-21 22:50:35 -0800

    add more tests

--- app/decorators/user_subscription_decorator.rb
+++ app/decorators/user_subscription_decorator.rb
@@ -1,13 +1,12 @@
 class UserSubscriptionDecorator < Draper::Decorator
 	delegate_all
 
-  def readable_time
+  def readable_hour
   	remind_hour == 99 ? "Random" : Time.parse("#{remind_hour}:00").strftime("%l %P")  	  
   end
 
   def subscription_days
-  	(1..7).select do |d|
-  		self.send('send_day_'+d.to_s)
-  	end.join "/"
+  	days = (1..7).select {|d| self.send('send_day_'+d.to_s) }
+  	days.map{|d|Date::ABBR_DAYNAMES[d-1]}.join " "
   end
 end
