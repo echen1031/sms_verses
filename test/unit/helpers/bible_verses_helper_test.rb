04f49c9dbf308e6b34adfc1158c387a7349e613a
Alex Chen <alexchen@Alexs-MacBook-Air.local>
2013-12-21 16:31:53 -0800

    remove test folder

--- test/unit/helpers/bible_verses_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class BibleVersesHelperTest < ActionView::TestCase
-end
--- test/unit/helpers/user_subscriptions_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class UserSubscriptionsHelperTest < ActionView::TestCase
-end
