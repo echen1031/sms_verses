7a5e4c9eca9cd765f28927efa6e98534f519c749
Eric Chen <eric87chen@gmail.com>
2014-02-10 20:42:23 -0500

    rspec test examples for practices

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,5 +6,15 @@ describe User do
 		it "validates" do 
 			expect(user).to be_valid
 		end
+
+                it "validates an email address" do
+                        user.email = " "
+                        expect(user).to_not be_valid
+                end
+
+                it "validates the format of an email address" do
+                        user.email = "ericchen"
+                        user.valid?.should == false
+                end
 	end
 end
