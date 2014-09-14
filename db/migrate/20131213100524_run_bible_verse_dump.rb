e43dfdd2944ed8c9de7278e93820317ed81b33cd
Alex Chen <alexchen@Alexs-MacBook-Air-2.local>
2014-09-13 23:10:23 -0700

    modify

--- db/migrate/20131213100524_run_bible_verse_dump.rb
+++ db/migrate/20131213100524_run_bible_verse_dump.rb
@@ -2,12 +2,12 @@ class RunBibleVerseDump < ActiveRecord::Migration
   def up
   	BibleVerse.delete_all
   	sql = ""
-	source = File.new("db/bible_verses.sql", "r")
-	while (line = source.gets)
-  		sql << line
-	end
-	source.close
-	execute sql
+  	source = File.new("db/bible_verses.sql", "r")
+  	while (line = source.gets)
+    	sql << line; nil
+  	end
+	  source.close
+	  execute sql; nil
   end
 
   def down
